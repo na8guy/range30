e1da84f7bbb22cfd87a79198cc8ad4fe82af8897
NA8GUY <wood.tyna@gmail.com>
2025-07-20 20:16:41 +0100

    v15

--- server.js
+++ server.js
@@ -61,9 +61,9 @@ app.get('/api/firebase-config', (req, res) => {
   try {
     const firebaseConfig = {
       apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: "range30-b324b.firebaseapp.com",
-      projectId: "range30-b324b",
-      storageBucket: "range30-b324b.appspot.com",
+      authDomain: "range30trips.firebaseapp.com",
+      projectId: "range30trips",
+      storageBucket: "range30trips.firebasestorage.app",
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.FIREBASE_APP_ID,
       vapidKey: process.env.FIREBASE_VAPID_KEY
